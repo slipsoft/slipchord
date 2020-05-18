f2afbd595cabe28dd3df25d7817a0c7baa07244c
n-peugnet <n.peugnet@free.fr>
2020-05-18 23:03:39 +0200

    integrate search code into ex3

--- ex3/main.c
+++ ex3/main.c
@@ -26,31 +26,37 @@ int initiator;
  */
 void simulator()
 {
+	MPI_Status status;
 	int *peertable;
 	int nb_peers = NB;
+	int i = 0;
 	init_pairs_aleatoire_non_classe(&peertable, nb_peers, NB_KEY_EXP);
-	for (int i = 0; i < nb_peers; i++) {
+	for (i = 0; i < nb_peers - 1; i++) {
 		send_message(i, VALUE, &peertable[i], 1);
 	}
+	MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, INIT_END, MPI_COMM_WORLD, &status);
+	send_message(i, VALUE, &peertable[i], 1);
 	free(peertable);
 }
 
 void newpeer()
 {
 	MPI_Status status;
-	int payload_size = NB;
+	int payload_size = NB + 1;
 	int *payload = malloc(sizeof(int) * payload_size);
 	struct peer_data *data = create_peer_data(NB);
 
 	MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, VALUE, MPI_COMM_WORLD, &status);
 	data->ftsize = NB_KEY_EXP;
-	running = 0;
 
-	while (running) {
-		receive_message(&status, payload, payload_size);
-		if (status.MPI_TAG == INIT) {
-		}
-	}
+	int chord_next;
+	int rank_prev = rank - 1;
+	payload[0] = value + 1;
+
+	printf("P%d> Started with value %d payload_size = %d\n", rank, value, payload_size);
+	MPI_Send(payload, payload_size, MPI_INT, rank_prev, TAG_RECHERCHE_INIT, MPI_COMM_WORLD);
+	MPI_Recv(&chord_next, 1, MPI_INT, rank_prev, TAG_RECHERCHE_TROUVE, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+	printf("FIND NEXT CHORD PEER: %d", chord_next);
 
 	delete_peer_data(data);
 }
@@ -68,6 +74,13 @@ void peer()
 	struct pair *peer_table;
 	struct peer_data *data = create_peer_data(NB);
 
+	int valeur_clef;
+	// à qui renvoyer le message lorsque le pair responsable est trouvé
+	int rang_retour_message;
+	int doit_renvoyer_message;
+	// Pour factoriser les fonctions, vrai si je dois rechercher un pair
+	int doit_rechercher_clef = 0;
+
 
 	srand(getpid());
 
@@ -77,7 +90,7 @@ void peer()
 	MPI_Recv(&value, 1, MPI_INT, MPI_ANY_SOURCE, VALUE, MPI_COMM_WORLD, &status);
 	data->ftsize = NB_KEY_EXP;
 
-	printf("P%d> Started with value %d\n", rank, value);
+	printf("P%d> Started with value %d payload_size = %d\n", rank, value, payload_size);
 
 
 	if (initiator) {
@@ -90,6 +103,7 @@ void peer()
 
 	while (running) {
 		receive_message(&status, payload, payload_size);
+		valeur_clef = payload[0];
 		if (status.MPI_TAG == INIT) {
 			// peer_table initialization message.
 			if (initiator && payload[token_index] < rank) {
@@ -118,6 +132,7 @@ void peer()
 			if (!winner) {
 				send_message(right, DONE, payload, NB);
 			} else {
+				send_message(NB + 1, INIT_END, payload, 1);
 				send_message(right, PRINT, payload, 1);
 			}
 		} else if (status.MPI_TAG == REVERSE) {
@@ -132,11 +147,87 @@ void peer()
 			} else {
 				send_message(right, END, payload, 1);
 			}
-		} else if (status.MPI_TAG == END) {
-			running = 0;
-			if (!winner) {
-				send_message(right, END, payload, 1);
+		// } else if (status.MPI_TAG == END) {
+		// 	running = 0;
+		// 	if (!winner) {
+		// 		send_message(right, END, payload, 1);
+		// 	}
+		} else if (status.MPI_TAG == TAG_RECHERCHE_INIT) {
+			// Le processus init me demande de rechercher une clef
+			doit_rechercher_clef = 1;
+			doit_renvoyer_message = 0;
+			rang_retour_message = status.MPI_SOURCE; // le message devra m'être retourné// Je recherche le processus à qui envoyer ma demande
+			printf("(pair %d rg %d) : Simulateur me demande de rechercher la clef %d\n\n", value, rank, valeur_clef);
+		} else if (status.MPI_TAG == TAG_RECHERCHE_PAIR) {
+			// Un pair me demande de rechercher une clef
+			doit_rechercher_clef = 1;
+			doit_renvoyer_message = 1;
+			// Je reçois le rang duquel je vais devenoir recevoir un message
+			MPI_Recv(&rang_retour_message, 1, MPI_INT, status.MPI_SOURCE, TAG_RECHERCHE_PAIR, MPI_COMM_WORLD, &status);
+		}
+		
+		
+		if (doit_rechercher_clef) {
+			int resp_index = trouver_index_responsable(data->ftable,
+				NB_KEY_EXP, valeur_clef, value);
+			
+			int resp_valeur;
+			int resp_rang;
+			
+			if (resp_index == -2) {
+				// Je suis responsable de la clef
+				resp_valeur = value;
+				resp_rang = rank;
+				printf("(pair %d rg %d) : je suis responsable de la clef(%d).\n\n", value, rank, valeur_clef);
+
+				
+			} else if (resp_index == -1) {
+				// Mon successeur est responsable de la clef
+				resp_valeur = data->ftable[0].valeur;
+				resp_rang = data->ftable[0].rang;
+				printf("(pair %d rg %d) : mon successeur(%d) est responsable de la clef(%d).\n\n",
+					value, rank, resp_valeur, valeur_clef);
+				
+			} else {
+				doit_renvoyer_message = 0; // ce n'est pas à moi de renvoyer le message, comme je passe le message.
+				// Il faut que j'envoie une demande à un pair pour trouver le responsable
+				printf("(pair %d rg %d) : j'envoie la demande au pair %d (rg %d) (recherche de la clef %d).\n\n",
+					value, rank, data->ftable[resp_index].valeur, data->ftable[resp_index].rang, valeur_clef);
+				
+				// Envoi de la demande au pair
+				int dest = data->ftable[resp_index].rang;
+				MPI_Send(&valeur_clef, 1, MPI_INT, dest, TAG_RECHERCHE_PAIR, MPI_COMM_WORLD);
+				
+				// Envoi du rang à qui renvoyer le message, une fois le responsable trouvé,
+				// pour que le message puisse revenir directement au pair qui a fait la permière demande.
+				MPI_Send(&rang_retour_message, 1, MPI_INT, dest, TAG_RECHERCHE_PAIR, MPI_COMM_WORLD);
+				
+				// Si je suis le processus qui a fait la demande initiale, j'attends la réponse
+				// if (rang_retour_message == rank)
+				if (status.MPI_TAG == TAG_RECHERCHE_INIT) {
+					// Réception de la réponse (du pair dont le successeur est responsable de la clef)
+					MPI_Recv(&resp_valeur, 1, MPI_INT, MPI_ANY_SOURCE, TAG_RECHERCHE_TROUVE, MPI_COMM_WORLD, &status);
+					MPI_Recv(&resp_rang, 1, MPI_INT, status.MPI_SOURCE, TAG_RECHERCHE_TROUVE, MPI_COMM_WORLD, &status);
+					printf("(pair %d rg %d) : réception (de rg %d) du pair responsable de la clef : valeur(%d) rang_MPI(%d).\n\n",
+						value, rank, status.MPI_SOURCE, resp_valeur, resp_rang);
+				}
 			}
+			
+			
+			
+			// Si je suis le processus qui a fait la demande initiale,
+			// ma demande a été bloquante, du coup là j'envoie un message au processus
+			// init.
+			//if (status.MPI_TAG == TAG_RECHERCHE_INIT) {
+			if (doit_renvoyer_message) {
+				MPI_Send(&resp_valeur, 1, MPI_INT, rang_retour_message, TAG_RECHERCHE_TROUVE, MPI_COMM_WORLD);
+				MPI_Send(&resp_rang, 1, MPI_INT, rang_retour_message, TAG_RECHERCHE_TROUVE, MPI_COMM_WORLD);
+			} else {
+				// i.e. (status.MPI_TAG == TAG_RECHERCHE_INIT)
+				// Envoi au simulateur (le simulateur va broadcast un message de terminaison)
+				MPI_Send(&resp_valeur, 1, MPI_INT, rang_retour_message, TAG_RECHERCHE_TROUVE, MPI_COMM_WORLD);
+			}
+			
 		}
 		
 	}
--- ex3/tags.h
+++ ex3/tags.h
@@ -5,4 +5,8 @@
 #define REVERSE       4 // Message to signal a reverse finger
 #define END           5 // Message to Quit the application
 #define PRINT         6 // Message to trigger a print
-#define START         7 // Message to start a proc
+#define INIT_END      7 // Message to notify the simulator that init is ended
+
+#define TAG_RECHERCHE_INIT   100 // recherche d'une clef : demande depuis le processus init
+#define TAG_RECHERCHE_PAIR   101 // recherche d'une clef : demande depuis un pair
+#define TAG_RECHERCHE_TROUVE 102 // recherche d'une clef : le responsable envoie que c'est lui
